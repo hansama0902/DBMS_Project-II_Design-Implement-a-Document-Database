2ad818713f334411d3eaca4ba38abf2e8d79b658
hansama0902 <20091247@marist.edu>
2024-11-14 14:56:58 -0800

    Litte change

--- nodeExpressMongoDBEJS_PatientManagementSystem/routes/index.js
+++ nodeExpressMongoDBEJS_PatientManagementSystem/routes/index.js
@@ -7,8 +7,6 @@ const { MongoClient} = require("mongodb");
 const uri = "mongodb://localhost:27017";
 const client = new MongoClient(uri);
 const dbName = "patient_management";
-
-
 // Add Patient
 router.post("/add", async (req, res) => {
   try {
